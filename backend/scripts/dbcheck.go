bf547151413a9ca8fe6389866c3f70f478555a22
NamalTharindu97 <NamalA701997@gmail.com>
2026-07-03 00:19:17 +0530

    fix: verify backend is running after start, add dbcheck diagnostic output

--- backend/scripts/dbcheck.go
+++ backend/scripts/dbcheck.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/nomnom-lk/backend/internal/config"
+	"github.com/nomnom-lk/backend/internal/database"
 )
 
 func main() {
@@ -18,21 +19,39 @@ func main() {
 		os.Exit(1)
 	}
 
+	fmt.Fprintf(os.Stderr, "[dbcheck] DATABASE_HOST=%s\n", cfg.Database.Host)
+	fmt.Fprintf(os.Stderr, "[dbcheck] DATABASE_PORT=%s\n", cfg.Database.Port)
+	fmt.Fprintf(os.Stderr, "[dbcheck] DATABASE_USER=%s\n", cfg.Database.User)
+	fmt.Fprintf(os.Stderr, "[dbcheck] DATABASE_NAME=%s\n", cfg.Database.Name)
+
 	addr := cfg.Database.Host + ":" + cfg.Database.Port
-	fmt.Fprintf(os.Stderr, "Attempting TCP connection to %s...\n", addr)
+	fmt.Fprintf(os.Stderr, "[dbcheck] TCP dial to %s...\n", addr)
 
 	conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "TCP dial error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "[dbcheck] TCP dial error: %v\n", err)
 		os.Exit(1)
 	}
 	conn.Close()
-	fmt.Fprintf(os.Stderr, "TCP connection OK\n")
+	fmt.Fprintf(os.Stderr, "[dbcheck] TCP connection OK\n")
 
 	dsn := cfg.Database.DSN()
-	fmt.Fprintf(os.Stderr, "DSN would be: host=%s port=%s user=%s dbname=%s sslmode=%s\n",
-		cfg.Database.Host, cfg.Database.Port, cfg.Database.User, cfg.Database.Name, cfg.Database.SSLMode)
-	_ = dsn
+	fmt.Fprintf(os.Stderr, "[dbcheck] DSN: %s\n", dsn)
 
-	fmt.Println("Config loaded successfully, database seems reachable")
+	fmt.Fprintf(os.Stderr, "[dbcheck] Attempting GORM connection...\n")
+	db := database.NewPostgresDB(&cfg.Database)
+	fmt.Fprintf(os.Stderr, "[dbcheck] GORM connection OK\n")
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[dbcheck] sql.DB error: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[dbcheck] Ping error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stderr, "[dbcheck] Ping OK\n")
 }
